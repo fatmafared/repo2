02ceced3c8c12da703d9920cae7cce77c88de36e
fatma fared <fatmafared23@gmail.com>
2020-02-06 22:26:40 +0200

    initial commit

---
+++ js/modal.js
@@ -0,0 +1,23 @@
+//get the modal//
+var modal =document.getElementById("myModal");
+//get the button that opens the modal
+var btn=document.getElementById("myBtn");
+//get the <span> element that closes the modal
+var span= document.getElementsByClassName("close")[0];
+//when the user clicks on the buttons,open the modal
+btn.onclick=function(){
+    modal.style.display= "block";  
+    
+}
+//when the user clicks on <span>,close the modal
+span.onclick=function(){
+    modal.style.display= "none";
+}
+//when the user clicks on anywhere outside the modal,close it
+window.onclick=function(event){
+    if (event.target==modal){
+        modal.style.display= "none";
+    }
+    
+    
+}
---
+++ modal.js
@@ -0,0 +1,7 @@
+//get the modal//
+var modal =document.getElementById("myModal");
+//get the button that opens the modal//
+var btn=document.getElementById("btn");
+//get the <span> element that closes the modal//
+
+
