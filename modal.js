96acb68a1811f7754f71253e5c2db2251eb44a09
fatma fared <fatmafared23@gmail.com>
2020-02-06 22:44:42 +0200

    initial commit

--- modal.js
+++
@@ -1,7 +0,0 @@
-//get the modal//
-var modal =document.getElementById("myModal");
-//get the button that opens the modal//
-var btn=document.getElementById("btn");
-//get the <span> element that closes the modal//
-
-
